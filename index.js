06819f86fb1c13626dcbde9d219079fae3dca88b
briankush <briankush05@gmail.com>
2025-09-06 16:47:17 +0300

    Successfully achieved all basic crud operations

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/', (req, res)=>{
 // fetch all users
 app.get('/api/users', async (req, res)=>{
     try {
-        await User.find();
-        res.status(200).json(User);
+        const users= await User.find();
+        res.status(200).json(users);
     } catch (error) {
         res.status(500).send("An error occured in fetching the users")
     }
@@ -26,21 +26,42 @@ app.get('/api/users', async (req, res)=>{
 // fetch a user by id
 app.get('/api/user/:id', async (req, res)=>{
     try {
-        const id = req.params;
-        await User.findById(id);
-        res.status(200).json(User);
+        const {id} = req.params;
+        const user= await User.findById(id);
+        res.status(200).json(user);
     } catch (error) {
-        res.status(500).send("An error occured in fetching the users")
+        res.status(500).send("An error occured in fetching the user")
     }
 });
 
 //Create a user
 app.post('/api/users', async (req, res)=>{
     try {
-         await User.create(req.body);
-        res.status(200).json(User);
+        await User.create(req.body);
+        res.status(200).send("User created successfully").json(User);
     } catch (error) {
-        res.status(500).send("An error occured in fetching the users")
+        res.status(500).send("An error occured in Creating the user")
+    }
+});
+
+// delete user by id
+app.delete('/api/user/:id', async (req, res)=>{
+    try {
+        const {id}= req.params
+        await User.findByIdAndDelete(id);
+        res.status(200).send("User deleted successfully");
+    } catch (error) {
+        res.status(500).send("User deletion failed")
+    }
+});
+
+// delete all users
+app.delete('/api/users', async (req, res)=>{
+    try {
+        await User.deleteMany();
+        res.status(200).send("Users deleted successfully");
+    } catch (error) {
+        res.status(500).send("User deletion failed")
     }
 });
 
